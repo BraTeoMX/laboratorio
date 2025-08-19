5083c574f8453ca7c36979caadfaad1fd3077a14
Brayam Programador-BraTeoMX <brateomx@gmail.com>
2025-08-18 19:01:13 -0600

    correcion del titulo

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -210,7 +210,9 @@ new class extends Component {
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-teal-950 overflow-hidden shadow-sm sm:rounded-lg">
-                <h2>Administrador de usuarios</h2>
+                <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-200 mb-4">
+                    Administrador de usuarios
+                </h2>
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
                     <div class="flex justify-between items-center mb-4">
